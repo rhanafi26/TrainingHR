6284060535416b942ffd77b0580de16e8c81cbce
Riza Hanafi <rizahanafi98@gmail.com>
2026-08-21 18:38:42 +0700

    Add SOCKET_URL configuration based on environment

--- src/config.js
+++ src/config.js
@@ -2,3 +2,7 @@
 export const API_URL = import.meta.env.VITE_API_URL 
   ? `${import.meta.env.VITE_API_URL}/api` 
   : 'http://localhost:5001/api';
+
+export const SOCKET_URL = import.meta.env.VITE_API_URL 
+  ? import.meta.env.VITE_API_URL 
+  : 'http://localhost:5001';
